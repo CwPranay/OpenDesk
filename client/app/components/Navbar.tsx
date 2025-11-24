975a57a1b7775d59e9eb9653398d2249a452e5de
Pranay Gurav <guravpranay81@gmail.com>
2025-11-24 22:30:10 +0530

    project route hide from the frontend to non login users

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -9,7 +9,10 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 
+
 export default function Navbar() {
+
+
   return (
     <nav className="sticky top-0 z-50 bg-slate-900/80 backdrop-blur-xl border-b border-indigo-500/20 shadow-lg shadow-indigo-500/10">
       <div className="max-w-7xl mx-auto h-16 flex items-center justify-between px-6">
@@ -33,13 +36,17 @@ export default function Navbar() {
             Home
             <span className="absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-0.5 bg-indigo-400 group-hover:w-3/4 transition-all duration-300"></span>
           </Link>
-          <Link
-            href="/projects"
-            className="px-4 py-2 rounded-lg text-gray-300 hover:text-white hover:bg-indigo-500/10 transition-all duration-200 relative group"
-          >
-            Projects
-            <span className="absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-0.5 bg-indigo-400 group-hover:w-3/4 transition-all duration-300"></span>
-          </Link>
+
+          <SignedIn>
+            <Link
+              href="/projects"
+              className="px-4 py-2 rounded-lg text-gray-300 hover:text-white hover:bg-indigo-500/10 transition-all duration-200 relative group"
+            >
+              Projects
+              <span className="absolute bottom-0 left-1/2 -translate-x-1/2 w-0 h-0.5 bg-indigo-400 group-hover:w-3/4 transition-all duration-300"></span>
+            </Link>
+          </SignedIn>
+
           <Link
             href="/projects/explore"
             className="px-4 py-2 rounded-lg text-gray-300 hover:text-white hover:bg-indigo-500/10 transition-all duration-200 relative group"
@@ -87,9 +94,9 @@ export default function Navbar() {
                 <span className="text-lg leading-none">+</span>
                 New Project
               </Link>
-              
-                <UserButton afterSignOutUrl="/" />
-              
+
+              <UserButton afterSignOutUrl="/" />
+
             </div>
           </SignedIn>
         </div>
