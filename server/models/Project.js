d0d85711afa720e805d352bcc4e5000fab3d115f
Pranay Gurav <guravpranay81@gmail.com>
2025-11-26 21:50:24 +0530

    added language to project model

--- server/models/Project.js
+++ server/models/Project.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const projectShema = new mongoose.Schema({
     title:{type:String,required:true},
     description:String,
-     repoUrl: { type: String },
+    repoUrl: { type: String },
+    language: { type: String, default: "Unknown", index: true },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     members:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
     createdAt:{type:Date,default:Date.now},
