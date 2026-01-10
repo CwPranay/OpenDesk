78221d471fc0e8233be6b1d320c8f4f1204339ba
Pranay Gurav <guravpranay81@gmail.com>
2026-01-10 09:53:19 +0530

    fix  enforce Clerk auth middleware and headers

--- server/index.js
+++ server/index.js
@@ -5,16 +5,23 @@ import mongoose from "mongoose";
 import taskRoutes from "./routes/taskRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import { requireAuth } from "./middleware/auth.js";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
 const app = express();
 
 app.use(cors({
   origin: ["http://localhost:3000","https://opendesk-com.vercel.app"],
   methods: ["GET", "POST", "PATCH", "DELETE"],
+ allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 }));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("AUTH HEADER:", req.headers.authorization);
+  next();
+});
+
 
 // MongoDB connect
 mongoose.connect(process.env.MONGODB_URI)
@@ -24,11 +31,8 @@ mongoose.connect(process.env.MONGODB_URI)
 // Public test route
 app.get("/api/hello", (req, res) => res.json({ message: "OpenDesk : The Collabration Tool" }));
 
-// ⛔ REMOVE THIS — it breaks explore
-// app.use("/api", requireAuth);
 
-// 👉 APPLY auth only inside private routes (these routes already check inside)
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
+app.use("/api/projects",ClerkExpressRequireAuth(), projectRoutes);
+app.use("/api/tasks",ClerkExpressRequireAuth(), taskRoutes);
 
 app.listen(5000, () => console.log("🚀 Server running on port 5000"));
